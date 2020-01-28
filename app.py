81c59012d7653bcf4e79e1e4b52f238eec3df8fe
Reinhold83 <raphaelreinhold@hotmail.com>
2020-01-28 21:10:10 +0000

    deployedDone

--- app.py
+++ app.py
@@ -10,6 +10,6 @@ def index():
 		name = "Reinhold"
 	return render_template("index.html", name=name)
 
-if __name__ == '__main__':
-	app.run(host='127.0.0.1', port=8080, debug=True)
+#if __name__ == '__main__':
+	#app.run(host='127.0.0.1', port=8080, debug=True)
 	#serve(app, host='0.0.0.0', port=8080)
